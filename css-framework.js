1ea21a1ab5beccf69e5acd6ffc3d83038958f26c
sanjwilli <williamsanjay77@gmail>
2018-12-27 17:03:15 -0500

    Updated Dropdown 0.1

--- css-framework.js
+++ css-framework.js
@@ -1,3 +1,11 @@
-$.getScript('components/dropdown.js', function() {
+/*$.getScript('components/dropdown.js', function() {
 
+});*/
+
+$(document).ready(function (){
+	$('div.dropdown').click(function(){
+		var dropdown = this;
+		$(dropdown).toggleClass('active').toggleClass('visible');
+		$(dropdown).find('.menu').first().toggleClass('show').toggleClass('visible hidden');
+	});
 });
