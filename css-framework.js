0b970da33a608ca0ccd11730b4418bb8c9d60762
sanjwilli <williamsanjay77@gmail>
2018-12-28 17:04:40 -0500

    Updated Dropdown 0.2

--- css-framework.js
+++ css-framework.js
@@ -3,9 +3,39 @@
 });*/
 
 $(document).ready(function (){
+	/* 	
+		*********************
+		*					*
+		*	DropDown Menu	*
+		*					*
+		*********************
+	*/
+
 	$('div.dropdown').click(function(){
-		var dropdown = this;
-		$(dropdown).toggleClass('active').toggleClass('visible');
-		$(dropdown).find('.menu').first().toggleClass('show').toggleClass('visible hidden');
+		
+		var 
+
+			dropDown = this,
+			pageHeight = $(window).height(),
+			dropDownTop = $(this).offset().top,
+			abovePage = 0.84 * $(window).height(),
+			topTolerance = 5
+
+		;
+
+
+		if ((dropDownTop + topTolerance) >=  abovePage && (dropDownTop + topTolerance) <= pageHeight) {
+
+			$(dropDown).toggleClass('active').toggleClass('upward').toggleClass('visible');
+
+		}else {
+
+			$(dropDown).toggleClass('active').toggleClass('visible');
+		}
+
+		$(dropDown).find('.menu').first().toggleClass('show').toggleClass('visible hidden');
+
+		console.log(dropDownTop);
+		console.log(abovePage);
 	});
 });
